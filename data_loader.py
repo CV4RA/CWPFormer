ac4cd2f10fba88e018a2fcc45293009b75ce541d
CV4RA <163392679+CV4RA@users.noreply.github.com>
2024-10-24 03:14:59 +0000

    cv4ra

--- data_loader.py
+++ data_loader.py
@@ -7,8 +7,8 @@ def get_dataset(batch_size):
         transforms.ToTensor(),
     ])
     
-    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    val_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train_dataset = datasets.VPRICE(root='./data', train=True, download=True, transform=transform)
+    val_dataset = datasets.VPRICE(root='./data', train=False, download=True, transform=transform)
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
